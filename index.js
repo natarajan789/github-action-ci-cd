84f6f7ea79ca3419681da9245251833b092427e9
natarajan789 <natarajan2533@gmail.com>
2026-05-11 23:05:57 +0530

    Update response message in root route

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.status(200).json({ status: "ok", message: "Hello Github Action Working V1"});
+  res.status(200).json({ status: "ok", message: "Hello Github Action Working V1 check"});
 });
 
 app.get("/health", (req, res) => {
